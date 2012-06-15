76554616779c27b0d741eb17cb277d93c19cbf18
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2012-06-15 22:24:50 +0200

    Adding sample settings file

---
+++ inc/settings.sample.php
@@ -0,0 +1,9 @@
+<?php
+$harvest_user = ''; // Your Harvest username, usually an email address
+$harvest_pass = ''; // Your Harvest password
+$harvest_account = ''; // The {myaccount} portion of your Harvest url: {myaccount}.harvestapp.com
+// date_default_timezone_set('America/Los_Angeles'); // Set your timezone if it is not set in your php.ini
+
+// number of working hours a day you want to measure against
+$working_hours_per_day = 7.5;
+?>
