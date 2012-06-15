0e67aae00448ad539c6a87d07c083fc3ec482687
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2012-06-15 22:35:22 +0200

    Removing the getWorkingDays function from index.php Refactored to inc/functions.php

--- index.php
+++ index.php
@@ -27,29 +27,6 @@ include('inc/functions.php');
     <img id="preloader" src="img/ajax-loader.gif" alt="">
     
     <div id="container">
-    
-      <?php
-        
-        function getWorkingDays($period)
-        {
-          $startDate = date('Y-m-01');
-          
-          if($period == 'month') $endDate = date('Y-m-t');
-          else $endDate = date('Y-m-d');
-
-          // Sets the Count
-          $count = 0;
-          $startStt = strtotime($startDate);
-          $endStt = strtotime($endDate);
-          // iterates through each day till the end day is back at the start date
-          while (date("Y-m-d", $startStt) <= date("Y-m-d",$endStt)){
-            $count = (date("w", $endStt) != 0 && date("w", $endStt) != 6) ? $count +1 : $count;
-            $endStt = $endStt - 86400;
-          }
-          return $count;
-        }
-      
-      ?>
       
       <p class="month"><?php echo date('F Y'); ?></p>
       
