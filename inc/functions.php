7565ff76b2ff46f3e3c0c2a9e00fccaefd776b14
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2012-07-24 22:37:23 +0200

    Adding new helper functions for calculating working hours etc

--- inc/functions.php
+++ inc/functions.php
@@ -19,4 +19,54 @@
     return $count;
   }
 
+  function getActualWorkingHoursInRange($config,$employees,$range = null) {
+
+      $days = getWorkingDays($range);
+      $hours = 0;
+
+      foreach ($employees as $user) {
+        if(isset($config["working_hours_per_day"][$user->email]) && $config["working_hours_per_day"][$user->email] >= 0) {
+          $hours += $config["working_hours_per_day"][$user->email] * $days;
+        }
+        else {
+          $hours += $config["working_hours_per_day"]["default"] * $days;
+        }
+      }
+
+      return round($hours);
+  }
+
+  function determineRankingGroup($hours_registered, $hours_goal) {
+    $performance = round($hours_registered/$hours_goal*100);
+
+    if($performance >= 105) {
+      $group = "A-karmahunter";
+    } elseif ($performance < 105 && $performance >= 95) {
+      $group = "B-goalie";
+    } elseif ($performance < 95 && $performance >= 70) {
+      $group = "C-karmauser";
+    } else {
+      $group = "D-slacker";
+    }
+    return $group;
+  }
+
+  function sortByOneKey(array $array, $key) {
+
+      $result = array();
+      $values = array();
+
+      foreach ($array as $id => $value) {
+          $values[$id] = isset($value[$key]) ? $value[$key] : '';
+      }
+
+      asort($values);
+
+      foreach ($values as $key => $value) {
+          $result[] = $array[$key];
+      }
+
+      return $result;
+  }
+
 ?>
